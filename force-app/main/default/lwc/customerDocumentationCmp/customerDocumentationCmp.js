0b715151691e8e88c35a9149c47c8aad9520e05a
Griffin Markay <griffin.markay@thrivent.com>
2026-03-25 16:59:31 -0400

    added button

--- force-app/main/default/lwc/customerDocumentationCmp/customerDocumentationCmp.js
+++ force-app/main/default/lwc/customerDocumentationCmp/customerDocumentationCmp.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
 
-export default class CustomerDocumentationCmp extends LightningElement {}
+export default class CustomerDocumentationCmp extends LightningElement {
+    handleHello(){
+        console.log('  Hey*** '   );
+    }
+}
