04ac9aca850567c5a58bb1fc9a4271fc5240b2ee
Griffin Markay <griffin.markay@thrivent.com>
2026-03-27 09:19:11 -0400

    cust doc

--- force-app/main/default/lwc/customerDocumentationCmp/customerDocumentationCmp.js
+++ force-app/main/default/lwc/customerDocumentationCmp/customerDocumentationCmp.js
@@ -1,7 +1,48 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { CloseActionScreenEvent } from 'lightning/actions';
+import { createRecord } from 'lightning/uiRecordApi';
 
 export default class CustomerDocumentationCmp extends LightningElement {
-    handleHello(){
-        console.log('  Hey*** '   );
+    @api recordId;
+
+    cdName;
+    cdDate;
+    cdReferal;
+    cdDescription;
+
+
+    handleNameChange(event) {
+        this.cdName = event.target.value;
+    }
+
+    handleDate(event){
+        this.cdDate - event.target.value
+    }
+
+    handleRef(event){
+        this.cdReferal - event.target.value
+    }
+    handleDesc(event){
+        this.cdDescription - event.target.value
+    }
+
+    async createCd() {
+        const fields = {Name: this.cdName, ActivityDate__c: this.cdDate, IsReferralAsked__c: this.cdReferal, PrimaryAccountId__c: this.recordId};
+        const recordInput = { apiName: 'CustomerDocumentation__c' , fields };
+        try{
+            await createRecord(recordInput);
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Client Documentation created',
+                    variant: 'success'
+                })
+            );
+          this.dispatchEvent(new CloseActionScreenEvent());
+        }catch(error){
+            console.log(' error *** ',  error );
+        }
+
     }
 }
